5d9c064e023ff878f2ccd9aad125028e8f6cf25a
leo kovalczuk <140639530+leo-kovalczuk@users.noreply.github.com>
2025-05-28 14:14:48 -0300

    65

--- main.js
+++ main.js
@@ -11,4 +11,24 @@ for (let i=0; i < botoes.length; i++){
     }
 }
 const contadores = document.querySelectorAll("contador");
-const tempoObjetivol = new Date ("2026-01-01T00:00:00")
+const tempoObjetivol = new Date ("2026-01-01T00:00:00");
+const tempoObjetivo2 = new Date ("2027-03-06T00:00:00");
+const tempoObjetivo3 = new date  ("2028-07-06T00:00:00");
+const tempoObjetivo4 = new Date ("2030-09-01T00:00:00");
+const tempos = [tempoObjetivol, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4]
+function atualizaCronometro (){
+    for (let i =0; < contadores.length; i++){
+        document.getElementById ("dias"+i).textContent = calculatempo(tempos {i}){0};
+        document.getElementById ("horas"+i).textContent = calculatempo(tempos {i}){1};
+        document.getElementById ("min"+i).textContent = calculatempo(tempos {i}){2};
+        document.getElementById ("seg"+i).textContent = calculatempo(tempos {i}){3};  
+    }
+    for(let i = 0; i < contadores.length; i++){
+        //contadores[i].textcontent = calculaTempo (tempos[i]);
+    }
+}
+function comecaCronometro(){
+    atualizaCronometro();
+    setInterval(atualizaCronometro, 1000);
+}
+comecaCronometro();
