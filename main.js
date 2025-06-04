dce55c31970857fe2cb6e3f12d2c0d08c6d8cf6f
leo kovalczuk <140639530+leo-kovalczuk@users.noreply.github.com>
2025-06-04 08:07:30 -0300

    33

--- main.js
+++ main.js
@@ -15,13 +15,13 @@ const tempoObjetivol = new Date ("2026-01-01T00:00:00");
 const tempoObjetivo2 = new Date ("2027-03-06T00:00:00");
 const tempoObjetivo3 = new Date  ("2028-07-06T00:00:00");
 const tempoObjetivo4 = new Date ("2030-09-01T00:00:00");
-const tempos = [tempoObjetivol, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4]
+const tempos = [tempoObjetivol, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 function atualizaCronometro (){
-    for (let i =0; < contadores.length; i++){
-        document.getElementById ("dias"+i).textContent = calculatempo(tempos {i}){0};
-        document.getElementById ("horas"+i).textContent = calculatempo(tempos {i}){1};
-        document.getElementById ("min"+i).textContent = calculatempo(tempos {i}){2};
-        document.getElementById ("seg"+i).textContent = calculatempo(tempos {i}){3};  
+    for (let i = 0; i < contadores.length; i++){
+        document.getElementById ("dias"+i).textContent = calculatempo(tempos [i])[0];
+        document.getElementById ("horas"+i).textContent = calculatempo(tempos [i])[1];
+        document.getElementById ("min"+i).textContent = calculatempo(tempos [i])[2];
+        document.getElementById ("seg"+i).textContent = calculatempo(tempos [i])[3];  
     }
     for(let i = 0; i < contadores.length; i++){
         //contadores[i].textcontent = calculaTempo (tempos[i]);
