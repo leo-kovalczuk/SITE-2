ee4028ac5d9fa968a39b85b6b3a211974ba4fb7f
leo kovalczuk <140639530+leo-kovalczuk@users.noreply.github.com>
2025-06-04 08:11:31 -0300

    33

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ const tempoObjetivol = new Date ("2026-01-01T00:00:00");
 const tempoObjetivo2 = new Date ("2027-03-06T00:00:00");
 const tempoObjetivo3 = new Date  ("2028-07-06T00:00:00");
 const tempoObjetivo4 = new Date ("2030-09-01T00:00:00");
-const tempos = [tempoObjetivol, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
+const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 function atualizaCronometro (){
     for (let i = 0; i < contadores.length; i++){
         document.getElementById ("dias"+i).textContent = calculatempo(tempos [i])[0];
