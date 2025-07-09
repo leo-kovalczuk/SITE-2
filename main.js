934018b7dc90cd03f9a0ba0ae3b0bb7b125622f5
leo kovalczuk <140639530+leo-kovalczuk@users.noreply.github.com>
2025-07-09 15:06:40 -0300

    33

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ for (let i=0; i < botoes.length; i++){
 }
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date ("2026-01-22T00:00:00");
-const tempoObjetivo2 = new Date ("2025-08-04T00:00:00");
+const tempoObjetivo2 = new Date ("2026-08-04T00:00:00");
 const tempoObjetivo3 = new Date ("2027-12-22T00:00:00");
 const tempoObjetivo4 = new Date ("2030-09-22T06:00:00");
 const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
